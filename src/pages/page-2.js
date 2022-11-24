6e0e1332c60ca8bd0058caa42a7f8dce1bc459a4
Eric Tuovila <36007317+only-devices@users.noreply.github.com>
2022-11-24 12:46:22 -0500

    Rearranged, added new attributes/elements

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -6,21 +6,28 @@ import Layout from '../components/layout'
 const SecondPage = () => (
   <Layout>
     <h1>Welcome heading! (no masking)</h1>
-    <p data-cs-encrypt>This is a paragraph. (data-cs-encrypt)</p>
+    <p data-cs-encrypt>This is a paragraph. | attribute="data-cs-encrypt"</p>
     <div>
       <form action="/submit">
-        <label for="firstName">First name:</label>
+        <label for="firstName">First name: (field below has data-cs-encrypt)</label>
         <input type="text" id="firstName-input" name="firstName" data-cs-encrypt />
         <input type="submit" value="Submit" />
       </form>
     </div>
-    <div class='contains-PII'>
-      Div starts here.
-      <p data-cs-mask><br />A paragraph within said div. (data-cs-mask)</p>
-      <img src="https://content.sportslogos.net/logos/53/75/full/6509_seattle_mariners-alternate-2015.png" width="400" alt="something" data-cs-encrypt />
+    <div class="contains-PII">
+      Div starts here. | class="contains-PII"
+      <p data-cs-mask><br />A paragraph within said div. | attribute=['data-cs-mask']</p>
     </div>
-
-    <Link to="/">Go back to the homepage</Link>
+    <div>
+      -------- No masking here! --------
+    </div>
+    <br/>
+    <div>
+      <img src="https://content.sportslogos.net/logos/53/75/full/6509_seattle_mariners-alternate-2015.png" width="200" alt="something" class="contains-PII" />
+    </div>
+    <div>
+      <Link to="/">Go back to the homepage</Link>
+    </div>
   </Layout>
 )
 
